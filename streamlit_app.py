d364260e7cf83b2322f3c769c8ee608ee17f6bd5
Jon-McKay <139435758+Jon-McKay@users.noreply.github.com>
2023-07-13 16:28:00 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as sl
 
 sl.title('My Parents New Healthy Diner')
 sl.header('Breakfast Menu')
-sl.text('Omega 3 & Blueberry Oatmeal')
-sl.text('Kale, Spinich & Rocket Smoothie')
-sl.text('Hard Boiled Free-Rnage Egg')
+sl.text('🥣 Omega 3 & Blueberry Oatmeal')
+sl.text('🥗 Kale, Spinich & Rocket Smoothie')
+sl.text('🐔 Hard Boiled Free-Rnage Egg')
+sl.text('🥑🍞 Avocado Toast')
