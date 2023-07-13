4073764d9cba045ed5a149f0ec85aa1a24fe78e0
Jon-McKay <139435758+Jon-McKay@users.noreply.github.com>
2023-07-13 16:55:51 +0930

    Update streamlit_app.py
    Bugfix - set_index() is not a setter !

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as sl
 import pandas as pd
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 sl.title('My Parents New Healthy Diner')
 sl.header('Breakfast Menu')
