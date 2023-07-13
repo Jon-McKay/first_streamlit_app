3af8ede3535252eade687b993d0c3f0e620fac8f
Jon-McKay <139435758+Jon-McKay@users.noreply.github.com>
2023-07-13 16:24:14 +0930

    Update streamlit_app.py
    Added header and menu text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
-import streamlit
+import streamlit as sl
 
-streamlit.title('My Parents New Healthy Diner')
+sl.title('My Parents New Healthy Diner')
+sl.header('Breakfast Menu')
+sl.text('Omega 3 & Blueberry Oatmeal')
+sl.text('Kale, Spinich & Rocket Smoothie')
+sl.text('Hard Boiled Free-Rnage Egg')
