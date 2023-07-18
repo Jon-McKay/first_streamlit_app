b47a3f6df4f1ca215610de19add883c56042062d
Jon-McKay <139435758+Jon-McKay@users.noreply.github.com>
2023-07-18 13:50:42 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalised = pd.json_normalize(fruityvice_response.json())
 sl.dataframe(fruityvice_normalised)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**sl.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
