1a25e714c4bc3641452f64a504299a0774059182
Jon-McKay <139435758+Jon-McKay@users.noreply.github.com>
2023-07-18 14:11:32 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,5 @@ my_data_rows = my_cur.fetchall()
 sl.header("The Fruit Load list contains:")
 sl.dataframe(my_data_rows)
 
-extra_fruit_list = my_data_rows.set_index('FRUIT_NAME')
-extra_fruit = sl.multiselect('Add another fruit?', extra_fruit_list)
-sl.write(extra_fruit, ' selected')
+extra_fruit = sl.text_input('Add another fruit?')
+sl.write(extra_fruit, ' added.  Thanks.')
